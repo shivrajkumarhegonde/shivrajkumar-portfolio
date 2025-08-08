40a0dc9a7a35d6469b03966790204df8060f1cec
shivrajkumarhegonde <shivrajkumarhegonde@gmail.com>
2025-08-09 01:52:25 +0530

    Configure PostgreSQL database

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -85,12 +85,13 @@ SENDGRID_SANDBOX_MODE_IN_DEBUG = False
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # This will use your live PostgreSQL database on Render,
+        # but will fall back to your local db.sqlite3 file for development.
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
